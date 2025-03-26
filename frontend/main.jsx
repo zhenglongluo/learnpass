d0a260504dc99eb4ab8e42ec95af8ecc17929f61
zhenglongluo <zhenglong@zhengloluodembp.newcastle.edu.au>
2025-03-26 17:08:05 +1100

    🐛 修复 AppRoutes 引用路径错误

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRoutes from "./AppRoutes";
+import AppRoutes from "./AppRoutes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
