d6074ee187e871a2ff83b5e3483dbdcc5b7ccf83
zhenglongluo <62728941+zhenglongluo@users.noreply.github.com>
2025-03-27 17:21:09 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://web-production-4aacb.up.railway.app', // ← 你的后端部署地址
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
 });
